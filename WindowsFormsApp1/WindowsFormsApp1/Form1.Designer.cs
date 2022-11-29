97af5d67b39af7874f40ac1fa7298cf1a871dce4
INSTALLER <INSTALLER@DESKTOP-R7O4L6O>
2022-11-29 14:58:09 +0900

    20221129

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -42,11 +43,20 @@ namespace WindowsFormsApp1
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(438, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(935, 442);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +67,7 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -25,47 +25,155 @@ namespace WindowsFormsApp1
             var workbook = new XLWorkbook(filePath);
             IXLWorksheet ws = workbook.Worksheet("振り分け購買依頼");
 
-            //仕入先
-            DataTable dt = new DataTable();
-            dt.Columns.Add("仕入先コード", typeof(string));
-            dt.Columns.Add("仕入先名称", typeof(string));
-            for (int rowIndex = 1;rowIndex <= 10000;rowIndex++)
-            {
-                DataRow dr = dt.NewRow();
-                dr["仕入先コード"] = rowIndex;
-                dr["仕入先名称"] = "名称"+rowIndex;
-                dt.Rows.Add(dr);
-            }
-            workbook.Worksheet("仕入先").Cell("A1").InsertTable(dt);
+            ////仕入先
+            //DataTable dt = new DataTable();
+            //dt.Columns.Add("仕入先コード", typeof(string));
+            //dt.Columns.Add("仕入先名称", typeof(string));
+            //for (int rowIndex = 1;rowIndex <= 10000;rowIndex++)
+            //{
+            //    DataRow dr = dt.NewRow();
+            //    dr["仕入先コード"] = rowIndex;
+            //    dr["仕入先名称"] = "名称"+rowIndex;
+            //    dt.Rows.Add(dr);
+            //}
+            //workbook.Worksheet("仕入先").Cell("A1").InsertTable(dt);
 
-            //仕入先
-            DataTable dt2 = new DataTable();
-            dt2.Columns.Add("品目コード", typeof(string));
-            dt2.Columns.Add("プラント", typeof(string));
-            dt2.Columns.Add("所要日", typeof(DateTime));
-            dt2.Columns.Add("標準仕入先コード", typeof(string));
+            ////仕入先
+            //DataTable dt2 = new DataTable();
+            //dt2.Columns.Add("品目コード", typeof(string));
+            //dt2.Columns.Add("プラント", typeof(string));
+            //dt2.Columns.Add("所要日", typeof(DateTime));
+            //dt2.Columns.Add("標準仕入先コード", typeof(string));
+            //for (int rowIndex = 1; rowIndex <= 10000; rowIndex++)
+            //{
+            //    DataRow dr = dt2.NewRow();
+            //    dr["品目コード"] = rowIndex;
+            //    dr["プラント"] = "PW20";
+            //    dr["所要日"] = new DateTime(2022, 11, 1);
+            //    dr["標準仕入先コード"] = rowIndex;
+            //    dt2.Rows.Add(dr);
+            //}
+            //workbook.Worksheet("SAP標準仕入れ先").Cell("A1").InsertTable(dt2);
+
+            //var rowRange = ws.Row(3);
+            //for (int rowIndex = 1; rowIndex <= 10000; rowIndex++)
+            //{
+            //    rowRange.CopyTo(ws.Row(rowIndex + 3));
+            //}
+
+            var rowRange = ws.Range(3, 1, 3, 15);
             for (int rowIndex = 1; rowIndex <= 10000; rowIndex++)
             {
-                DataRow dr = dt2.NewRow();
-                dr["品目コード"] = rowIndex;
-                dr["プラント"] = "PW20";
-                dr["所要日"] = new DateTime(2022, 11, 1);
-                dr["標準仕入先コード"] = rowIndex;
-                dt2.Rows.Add(dr);
+                rowRange.CopyTo(ws.Cell(rowIndex + 3, 1));
             }
-            workbook.Worksheet("SAP標準仕入れ先").Cell("A1").InsertTable(dt2);
 
-            //値セット
-            ws.Cell(3, 5).Style.NumberFormat.Format = "@";
-            ws.Cell(3, 5).Value = "1";
+            for (int rowIndex = 1; rowIndex <= 10000; rowIndex++)
+            {
+                var valueArray = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" };
+                ws.Cell(rowIndex + 2, 1).InsertData(new[] { valueArray });
 
-            //コピー
-            IXLRange range = ws.Range(3, 1, 3, 15);
-            //ペースト
-            ws.Cell(4, 1).Value = range;
-            //値セット
-            ws.Cell(4, 5).Style.NumberFormat.Format = "@";
-            ws.Cell(4, 5).Value = "2";
+                //////値セット
+                ////if (rowIndex > 1)
+                ////{
+                ////    //コピー
+                ////    IXLRange range = ws.Range(3, 5, 3, 5);
+                ////    ////ペースト
+                ////    ws.Cell(rowIndex + 2, 7).Value = range;
+                ////}
+
+                //IXLCell cell = ws.Cell(rowIndex + 2, 5);
+                //cell.Value = "1";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Protection.Locked = false;
+
+                //cell = ws.Cell(rowIndex + 2, 6);
+                //cell.Value = "2";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                ////if (rowIndex > 1)
+                ////{
+                ////    //コピー
+                ////    IXLRange range = ws.Range(3, 7, 3, 7);
+                ////    ////ペースト
+                ////    ws.Cell(rowIndex + 2, 7).Value = range;
+                ////}
+
+                //cell = ws.Cell(rowIndex + 2, 7);
+                //cell.Value = "3";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Protection.Locked = false;
+
+                //cell = ws.Cell(rowIndex + 2, 8);
+                //cell.Value = "4";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+
+                //////値セット
+                ////if (rowIndex > 1)
+                ////{
+                ////    //コピー
+                ////    IXLRange range = ws.Range(3, 9, 3, 9);
+                ////    ////ペースト
+                ////    ws.Cell(rowIndex + 2, 9).Value = range;
+                ////}
+                //cell = ws.Cell(rowIndex + 2, 9);
+                //cell.Value = "5";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                ////cell = ws.Cell(rowIndex + 2, 10);
+
+                //cell.Value = "6";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                //cell = ws.Cell(rowIndex + 2, 11);
+                //cell.Value = "7";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                //cell = ws.Cell(rowIndex + 2, 12);
+                //cell.Value = "8";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                //cell = ws.Cell(rowIndex + 2, 13);
+                //cell.Value = "9";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                //cell = ws.Cell(rowIndex + 2, 14);
+                //cell.Value = "10";
+                ////cell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                ////cell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+            }
+
+            ////コピー
+            //IXLRange range = ws.Range(3, 1, 3, 15);
+            ////ペースト
+            //ws.Cell(4, 1).Value = range;
+            ////値セット
+            //ws.Cell(4, 5).Style.NumberFormat.Format = "@";
+            //ws.Cell(4, 5).Value = "2";
 
             //保存
             workbook.SaveAs(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "TEST_NEW.xlsm"));
